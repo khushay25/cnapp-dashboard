a8abccb61423c896577bd9a4c1cc8615562e6667
khushipruthi_25 <khushi>
2024-08-28 01:17:03 +0530

    css update

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -35,7 +35,7 @@ function Heading({ category, addWidget, removeWidget, allCategories }) {
           <IoRefreshCircleOutline size={18} />
         </button>
         <button className="border-2 p-2 rounded-md border-gray-300 bg-white text-gray-400">
-          <IoMdMore />
+          <IoMdMore size={18} />
         </button>
         <button className="flex items-center border-2 p-2 rounded-md text-sm border-blue-900 bg-white text-blue-900 font-bold">
           <p className="border-r-2 border-blue-900 pr-2">
